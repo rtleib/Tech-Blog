5c9310262d5c70ebd22b78f4ee460007b2b6d93d
rtleib <rtleib@gmail.com>
2022-05-15 14:22:38 -0500

    added sequelize

--- server.js
+++ server.js
@@ -3,3 +3,6 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
