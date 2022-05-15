a6b4263c82ff1fa29588c1049a480bae53431cfc
rtleib <rtleib@gmail.com>
2022-05-15 14:28:10 -0500

    added sequelize

--- server.js
+++ server.js
@@ -6,3 +6,6 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
