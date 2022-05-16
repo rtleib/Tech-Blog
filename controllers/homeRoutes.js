4cdd6d7a055551dc170f2ff584d6d5aa291c4e37
rtleib <rtleib@gmail.com>
2022-05-15 21:38:41 -0500

    added post find all in home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -14,3 +14,36 @@ router.get('/', (req, res) => {
     }
   });
 });
+
+router.get('/', (req, res) => {
+    Post.findAll({
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        // pass a single post object into the homepage template
+        res.render('homepage', dbPostData[0]);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
