5dea6a809d59ff043081d4b8bcdb3d49293b3e81
rtleib <rtleib@gmail.com>
2022-05-15 22:40:09 -0500

    added const posts and updated render

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
+const posts = dbPostData.map((post) => post.get({ plain: true }));
 
 router.get('/', (req, res) => {
   res.render('homepage', {
@@ -10,40 +11,35 @@ router.get('/', (req, res) => {
     created_at: new Date(),
     comments: [{}, {}],
     user: {
-      username: 'test_user'
-    }
+      username: 'test_user',
+    },
   });
 });
 
 router.get('/', (req, res) => {
-    Post.findAll({
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-      ],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
-        },
-        {
+  Post.findAll({
+    attributes: ['id', 'post_url', 'title', 'created_at'],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
           model: User,
-          attributes: ['username']
-        }
-      ]
+          attributes: ['username'],
+        },
+      },
+      {
+        model: User,
+        attributes: ['username'],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      // pass a single post object into the homepage template
+      res.render('homepage', dbPostData[0].get({ plain: true }));
     })
-      .then(dbPostData => {
-        // pass a single post object into the homepage template
-        res.render('homepage', dbPostData[0]);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
